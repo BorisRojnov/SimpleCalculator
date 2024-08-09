46e6dc2236c34893364e24748cc972f9292d74d4
BorisRojnov <BorisRojnov@yandex.by>
2024-08-09 09:44:11 +0300

    Change all functional

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,20 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\t\t\t*** Welcome to calculator!***\n");
 
+        System.out.println("Введите первое число: ");
+        double firstNumber = new Scanner(System.in).nextDouble();
+        System.out.println("Введите второе число: ");
+        double secondNumber = new Scanner(System.in).nextDouble();
 
-        System.out.println("\t\t\t***Добро пожаловать в конвертер валют***");
-        System.out.println("Не забывайте при вводе чисел с плавающей точкой ставить знак \",\"\n");
-        System.out.println("Введите сумму в российских рублях:");
+        System.out.println("Сумма двух чисел:\n" + firstNumber + "+" + secondNumber + "="
+                + (firstNumber + secondNumber) );
+        System.out.println("Разность двух чисел:\n" + firstNumber + "-" + secondNumber + "="
+                + (firstNumber - secondNumber) );
+        System.out.println("Частное двух чисел:\n" + firstNumber + "/" + secondNumber + "="
+                + (firstNumber / secondNumber) + "\n");
 
-        double rub = new Scanner(System.in).nextDouble();
-
-        System.out.println("Введите актуальный курс доллара: ");
-        double dol = new Scanner(System.in).nextDouble();
-
-        System.out.println("Введите актуальный курс евро: ");
-        double eur = new Scanner(System.in).nextDouble();
-
-
-        System.out.println("В долларах: " + (rub * dol));
-        System.out.println("В евро: " + (rub * eur));
+        System.out.println("\t\t\t***Программа заверщена***");
     }
-
 }
